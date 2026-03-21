aba59040944fcee031e048cbabdb2518a4edf730
Zed Wong <kingflyme1@gmail.com>
2026-03-21 19:06:06 +0800

    chore: bump version to 0.1.1, add remote origin

--- src/cli.ts
+++ src/cli.ts
@@ -11,7 +11,7 @@ const program = new Command();
 program
   .name("hufi")
   .description("CLI tool for hu.fi DeFi platform")
-  .version("0.1.0");
+  .version("0.1.1");
 
 program.addCommand(createAuthCommand());
 program.addCommand(createExchangeCommand());
